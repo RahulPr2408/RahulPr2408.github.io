69b2118921245d67b792c0465f4b510b754c709c
Soumya Singh <singh.soumy@northeastern.edu>
2025-02-26 17:48:27 -0500

    pushgin responsive styling for navbar and footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import logo from "../assets/second-plate-logo-title.png"
+import logo from "../assets/second-plate-logo-title.png";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
       <nav className='navbar navbar-expand-lg navbar-dark custom-navbar'>
         <div className='container-fluid'>
+          {/* Logo */}
           <a className='navbar-brand' href='#home'>
             <img src={logo} alt="Second Plate Logo" className="navbar-logo" />
           </a>
 
-          {/* Middle part with navigation links */}
-          <div className="collapse navbar-collapse" id="navbarNav">
+          {/* 🟢 Hamburger Button for iPads & Mobile */}
+          <button className="navbar-toggler" onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? "✖" : "☰"}
+          </button>
+
+          {/* Navigation Links (Show dropdown on mobile, normal on desktop) */}
+          <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`} id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
                 <a className="nav-link" href="#home">Home</a>
@@ -30,7 +38,7 @@ const Navbar = () => {
             </ul>
           </div>
 
-          {/* Login Button */}
+          {/* Login Button (Same Design) */}
           <div className="d-flex">
             <button className="btn btn-outline-light">Login</button>
           </div>
