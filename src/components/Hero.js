e73ce7c4ed267aa228180330f018452a8c085982
Soumya Singh <singh.soumy@northeastern.edu>
2025-03-17 17:27:11 -0400

    adding more respinvie elements and mergin styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,6 @@ import BackgroundImage from '../assets/Home3.jpeg'; // Import background image
 const Hero = () => {
   return (
     <div className="hero-section">
-      <div className="hero-overlay"></div> {/* Optional overlay for better readability */}
       <div className="hero-content">
         <h1 className="hero-title">From Plate to Purpose <br /> Share, Don't Waste!</h1>
       </div>
