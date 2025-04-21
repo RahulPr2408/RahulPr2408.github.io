767543f369975f24794412b147691e8b15dfde8f
rahulsharma-fullstack <itsmerahul.contact@gmail.com>
2025-04-20 23:42:00 -0400

    testing deployment 1.5

--- src/components/dashboard/RestaurantDashboard.js
+++ src/components/dashboard/RestaurantDashboard.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
 import './RestaurantDashboard.css';
 import * as dashboardService from '../../services/dashboardService';
 
